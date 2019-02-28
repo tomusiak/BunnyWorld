03bbc91c31f449c710e380471bc170af8b48a5f1
tomusiak <atomusiak6@gmail.com>
2019-02-27 22:19:56 -0800

    Added adding new object into database functionality.

--- app/src/main/java/edu/stanford/cs108/bunnyworld/Database.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/Database.java
@@ -4,22 +4,26 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class Database extends SQLiteOpenHelper {
+class Database extends SQLiteOpenHelper {
+    private SQLiteDatabase db;
     private static final String DATABASE_NAME = "DB";
     private static Database instance;
+    private int numPages;
+
     public static synchronized Database getInstance(Context context) {
         if (instance == null) {
             instance = new Database(context.getApplicationContext());
         }
         return instance;
     }
+
     private Database(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
-    private int numPages;
-    @Override
+
     public void onCreate(SQLiteDatabase db) {
-        String createPageOne = "CREATE TABLE " + "Page1" + "(" +
+        this.db = db;
+        String createPageOne = "CREATE TABLE Page1 " + "(" +
                 "imgName TEXT PRIMARY KEY, " +
                 "text TEXT, " +
                 "hidden INTEGER, " +
@@ -41,11 +45,12 @@ public class Database extends SQLiteOpenHelper {
         db.execSQL(createInventory);
         numPages = 1;
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    public void createNewPage(SQLiteDatabase db) {
+    public void createNewPage() {
         String tableName = "Page" + Integer.toString(numPages);
         String createNewPage = "CREATE TABLE " + "PageOne" + "(" +
                 "imgName TEXT PRIMARY KEY, " +
@@ -60,7 +65,26 @@ public class Database extends SQLiteOpenHelper {
         numPages = numPages + 1;
     }
 
-    public void addNewObject(SQLiteDatabase db, Shape shape, Integer page) {
+    public void addNewObject(Shape shape, Integer page) {
+        String currentPage = "Page" + Integer.toString(page);
+        String shapeName = shape.imgName;
+        String shapeText = shape.text;
+        int shapeHidden = shape.hidden ? 1 : 0;
+        int shapeMoveable = shape.moveable ? 1 : 0;
+        double shapeX = shape.getX();
+        double shapeY = shape.getY();
+        double shapeWidth = shape.getWidth();
+        double shapeHeight = shape.getHeight();
+        String insertStr = "INSERT INTO " + currentPage + " VALUES "
+                + "('" + shapeName + "'," +
+                shapeText + "'," +
+                shapeHidden + "," +
+                shapeMoveable + "," +
+                shapeX + "," +
+                shapeY + "," +
+                shapeWidth + "," +
+                shapeHeight + ")";
+        db.execSQL(insertStr);
 
     }
 }
