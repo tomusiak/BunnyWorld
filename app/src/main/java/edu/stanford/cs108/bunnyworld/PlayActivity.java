e422e0380091e0db6f0d8fad99b92e2aacd47273
mei-lan <meilanes@stanford.edu>
2019-03-04 18:14:20 -0800

    Fixed the animation for the home screen and added functionality for the buttons

--- app/src/main/java/edu/stanford/cs108/bunnyworld/PlayActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/PlayActivity.java
@@ -1,13 +1,22 @@
 package edu.stanford.cs108.bunnyworld;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class PlayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
+        setContentView(R.layout.activity_editor);
     }
 }
