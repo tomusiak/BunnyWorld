63170de63442f479831aecdf81f7be5d4cb12684
isabellezhou <isabelle.jq.zhou@gmail.com>
2019-03-13 02:27:23 -0700

    Skeleton methods for PlayView

--- app/src/main/java/edu/stanford/cs108/bunnyworld/PlayView.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/PlayView.java
@@ -1,22 +1,55 @@
 package edu.stanford.cs108.bunnyworld;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * TODO: document your custom view class.
+ * Creates custom view for PlayActivity
  */
 public class PlayView extends View {
 
+    Shape selected;
+
     public PlayView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
+
+    /**
+     * Initializes starter images
+     */
+    private void init() {
+        selected = null;
+    }
+
+    /**
+     * Parses and executes script on shape
+     * @param shape
+     */
+    public void executeScript(Shape shape) {
+        // Insert script parsing currently in mainactivity
+    }
+
+    /**
+     * Renders the current page in question on the canvas by calling the
+     * page's render function.
+     */
+    public void drawPage(Canvas canvas) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
+
 
 }
