78e72c918813574eaa814416beb5431d7b73e864
mei-lan <meilanes@stanford.edu>
2019-03-15 06:36:34 -0700

    saving? buggy :(

--- app/src/main/java/edu/stanford/cs108/bunnyworld/MakeCharacter.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/MakeCharacter.java
@@ -1,13 +1,70 @@
 package edu.stanford.cs108.bunnyworld;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MakeCharacter extends AppCompatActivity {
+import java.util.UUID;
+
+public class MakeCharacter extends AppCompatActivity implements OnClickListener {
+
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_character);
+
+        saveButton = (Button)findViewById(R.id.save);
+        saveButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        setContentView(R.layout.activity_make_character);
+        final View drawView = findViewById(R.id.draw);
+
+        if (view.getId()==R.id.save){
+            AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+            saveDialog.setTitle("Save drawing");
+            saveDialog.setMessage("Save drawing to device Gallery?");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    drawView.setDrawingCacheEnabled(true);
+
+                    String imgSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), drawView.getDrawingCache(),
+                            UUID.randomUUID().toString()+".png", "drawing");
+                    if (imgSaved!=null) {
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    }
+                    else {
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+                    drawView.destroyDrawingCache();
+                }
+            });
+            saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            saveDialog.show();
+        }
+    }
+
 }
