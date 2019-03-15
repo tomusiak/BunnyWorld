af41e12d4aa75b42ee79d91c7713ab2c5d6e1eb6
mei-lan <meilanes@stanford.edu>
2019-03-15 04:01:49 -0700

    fixed the loading thing, made DrawView

---
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/DrawView.java
@@ -0,0 +1,26 @@
+package edu.stanford.cs108.bunnyworld;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Creates custom view for PlayActivity
+ */
+public class DrawView extends View {
+
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
