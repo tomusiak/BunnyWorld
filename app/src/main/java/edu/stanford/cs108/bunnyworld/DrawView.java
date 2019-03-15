14eaf83ebe15b341954deeb63e03bc05a67ff8eb
mei-lan <meilanes@stanford.edu>
2019-03-15 04:11:43 -0700

    drawing buttons

--- app/src/main/java/edu/stanford/cs108/bunnyworld/DrawView.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/DrawView.java
@@ -5,18 +5,27 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.ArrayList;
 
 /**
  * Creates custom view for PlayActivity
  */
 public class DrawView extends View {
 
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
