5c36748f0a02f4d4f7924a63f030c72d18cc3f8d
mei-lan <meilanes@stanford.edu>
2019-03-15 06:00:19 -0700

    very basic drawing app

--- app/src/main/java/edu/stanford/cs108/bunnyworld/DrawView.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/DrawView.java
@@ -17,11 +17,12 @@ import android.view.View;
 public class DrawView extends View {
 
     //drawing path
-    private Path drawPath;
+    private Path path;
+
     //drawing and canvas paint
-    private Paint drawPaint, canvasPaint;
-    //initial color
-    private int paintColor = 0xFF660000;
+    private Paint drawPaint;
+    private Paint canvasPaint;
+
     //canvas
     private Canvas drawCanvas;
     //canvas bitmap
@@ -29,7 +30,55 @@ public class DrawView extends View {
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
+    private void init() {
+        path = new Path();
+
+        drawPaint = new Paint();
+        drawPaint.setColor(Color.rgb(255,0,0));
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(path, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(path, drawPaint);
+                path.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
 
 }
