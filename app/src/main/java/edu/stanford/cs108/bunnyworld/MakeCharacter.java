4d1e3c07ade673e281d72e867ac7021bff3098a6
tomusiak <atomusiak6@gmail.com>
2019-03-15 20:06:28 -0700

    Merge branch 'master' of C:\Users\Alan\Dropbox\CS108\BunnyWorld with conflicts.

--- app/src/main/java/edu/stanford/cs108/bunnyworld/MakeCharacter.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/MakeCharacter.java
@@ -1,18 +1,8 @@
 package edu.stanford.cs108.bunnyworld;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
@@ -22,29 +12,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.UUID;
 
 public class MakeCharacter extends AppCompatActivity implements OnClickListener {
 
     private Button saveButton;
     private DrawView drawView;
-    private Bitmap savedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_character);
+
         saveButton = findViewById(R.id.save);
         saveButton.setOnClickListener(this);
+
         drawView = findViewById(R.id.drawView);
     }
 
@@ -54,29 +38,29 @@ public class MakeCharacter extends AppCompatActivity implements OnClickListener 
         if (view.getId()==R.id.save){
             AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
             saveDialog.setTitle("Save drawing");
-            saveDialog.setMessage("Save your character?");
-            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    //Define a bitmap with the same size as the view
-                    Bitmap b = Bitmap.createBitmap(drawView.getWidth(), drawView.getHeight(),Bitmap.Config.ARGB_8888);
-                    //Bind a canvas to it
-                    Canvas canvas = new Canvas(b);
-                    //Get the view's background
-                    Drawable bgDrawable =drawView.getBackground();
-                    if (bgDrawable!=null)
-                        //has background drawable, then draw it on the canvas
-                        bgDrawable.draw(canvas);
-                    else
-                        //does not have background drawable, then draw white background on the canvas
-                        canvas.drawColor(Color.WHITE);
-                    // draw the view on the canvas
-                    drawView.draw(canvas);
-                    //return the bitmap
-                    savedImage = b;
+            saveDialog.setMessage("Save drawing to device Gallery?");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    drawView.setDrawingCacheEnabled(true);
 
+                    String imgSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), drawView.getDrawingCache(),
+                            UUID.randomUUID().toString()+".png", "drawing");
+                    if (imgSaved!=null) {
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    }
+                    else {
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+                    drawView.destroyDrawingCache();
                 }
-            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
+            });
+            saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
                     dialog.cancel();
                 }
             });
@@ -84,10 +68,4 @@ public class MakeCharacter extends AppCompatActivity implements OnClickListener 
         }
     }
 
-    public Bitmap getCharacter () {
-        if (savedImage != null) {
-            return savedImage;
-        }
-        return null;
-    }
 }
